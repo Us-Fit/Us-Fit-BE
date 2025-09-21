3f39547cca0c7f72f2c866b074921b329b39f3a2
sehyeon <tpgusdla5789@gmail.com>
2025-09-22 01:16:04 +0900

    test: 서버 구동 후 작동 확인

---
+++ src/main/java/app/usfit/api/controller/HelloController.java
@@ -0,0 +1,13 @@
+package app.usfit.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello UsFit";
+    }
+}
