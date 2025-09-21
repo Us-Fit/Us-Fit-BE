ecaca7d65fe15795517653c62e8e2081e849ab56
sehyeon <tpgusdla5789@gmail.com>
2025-09-22 00:53:48 +0900

    Initial commit: UsFit Spring Boot

---
+++ src/main/java/app/usfit/api/UsFitApplication.java
@@ -0,0 +1,13 @@
+package app.usfit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/usfit/api/UsFitApplicationTests.java
@@ -0,0 +1,13 @@
+package app.usfit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
