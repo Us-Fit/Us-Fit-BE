21be071d169190b2e02aa857a13d8e517491ca44
sehyeon <tpgusdla5789@gmail.com>
2025-09-24 23:52:46 +0900

    test

--- src/main/java/app/usfit/api/UsFitApplication.java
+++ src/main/java/app/usfit/api/UsFitApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UsFitApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsFitApplication.class, args);
