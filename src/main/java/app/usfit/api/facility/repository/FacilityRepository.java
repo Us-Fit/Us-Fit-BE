dd822b0e0bc9ab3df3a0b5ff2b6de2cc5a30381f
sehyeon <tpgusdla5789@gmail.com>
2025-10-12 01:11:52 +0900

    feat: facility repository name, roadAddr1로 찾기 기능

--- src/main/java/app/usfit/api/facility/repository/FacilityRepository.java
+++ src/main/java/app/usfit/api/facility/repository/FacilityRepository.java
@@ -1,4 +1,20 @@
 package app.usfit.api.facility.repository;
 
-public interface FacilityRepository {
+import app.usfit.api.facility.entity.Facility;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.*;
+
+public interface FacilityRepository extends JpaRepository<Facility, Long> {
+    @Query("""
+      select f from Facility f
+      join f.addresses a
+      where f.name = :name
+        and a.roadAddr1 = :roadAddr1
+    """)
+    Optional<Facility> findByNameAndRoadAddr1(@Param("name") String name,
+                                              @Param("roadAddr1") String roadAddr1);
+
 }
