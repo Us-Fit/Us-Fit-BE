376ef51a4e86df3953d0191927f58f9813c27c6a
Jeong, Se-hyeon <168955357+gitIt-sehyeon@users.noreply.github.com>
2025-10-13 01:18:44 +0900

    chore: UsFitApplication.java test용 코드 삭제

--- src/main/java/app/usfit/api/UsFitApplication.java
+++ src/main/java/app/usfit/api/UsFitApplication.java
@@ -1,29 +1,11 @@
 package app.usfit.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import app.usfit.api.user.service.UserService;
-
 @SpringBootApplication
-public class UsFitApplication implements CommandLineRunner {
-	
-	@Autowired
-	private UserService userService;
-	
+public class UsFitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsFitApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// 애플리케이션 시작 시 User 테이블의 데이터 개수를 로그로 출력
-		long userCount = userService.countUsers();
-		System.out.println("===========================================");
-		System.out.println("📊 DATABASE CONNECTION TEST");
-		System.out.println("👥 Current User count in database: " + userCount);
-		System.out.println("===========================================");
 	}
 }
