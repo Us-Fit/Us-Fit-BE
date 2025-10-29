74d05c71fccc6b7e7d785d68090c922ee8cf3115
sehyeon <tpgusdla5789@gmail.com>
2025-10-30 00:01:44 +0900

    test: test code 작성-시, 도 기준으로 filtering이 잘 되는지 확인

--- src/test/java/app/usfit/api/facility/FindFacility.java
+++ src/test/java/app/usfit/api/facility/FindFacility.java
@@ -1,2 +1,29 @@
-package app.usfit.api.facility;public class FindFacility {
+package app.usfit.api.facility;
+
+import app.usfit.api.facility.repository.FacilityRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class FindFacility {
+    @Autowired
+    FacilityRepository repo;
+
+    @Test
+    @DisplayName("경기도의 모든 시설을 찾는다 facility id를 반환")
+    void findIdsBySido(){
+        List<Long> ids = repo.findFacilityIdsBySidoCd("4100000000");
+        System.out.println("facility ids = " + ids);
+        assertThat(ids).isNotNull();
+    }
 }
