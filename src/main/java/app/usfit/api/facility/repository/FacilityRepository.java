eaadbdf3d77b30b9f1047bb82b686b51d2c30e77
sehyeon <tpgusdla5789@gmail.com>
2025-10-29 23:51:04 +0900

    feat: facility filtering 을 위한 Query문 작성

--- src/main/java/app/usfit/api/facility/repository/FacilityRepository.java
+++ src/main/java/app/usfit/api/facility/repository/FacilityRepository.java
@@ -21,4 +21,29 @@ public interface FacilityRepository extends JpaRepository<Facility, Long> {
     List<Facility> findAllByNameAndRoadAddr1(@Param("name") String name,
                                              @Param("roadAddr1") String roadAddr1);
 
+    //facility id List를 넘겨주면 facility entity list 반환
+    @Query("""
+           select f
+           from Facility f
+           where f.id in :ids
+           order by f.id asc
+           """
+    )
+    List<Facility> findFacilityById(@Param("ids") Collection<Long> ids);
+
+    //시, 도 로 facility id 찾기
+    @Query("""
+           select distinct a.facility.id
+           from FacilityAddress a
+           where a.sidoCd = :sidoCd
+           """
+    )
+    List<Long> findFacilityIdsBySidoCd(@Param("sidoCd") String sidoCd);
+
+    //sport id에 맞는 facility id 찾기
+//    @Query("""
+//
+//           """
+//    )
+//    List<Long> findFacilityIdsBySport(@Param("sportId") )
 }
