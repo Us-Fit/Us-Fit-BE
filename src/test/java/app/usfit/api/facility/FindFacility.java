a0a7ac6d07686caa82eb6b3ede2d24c3761ca89d
sehyeon <tpgusdla5789@gmail.com>
2025-11-02 03:25:05 +0900

    test: facility 찾기 test code 작성 api로 바로 사용 가능

--- src/test/java/app/usfit/api/facility/FindFacility.java
+++ src/test/java/app/usfit/api/facility/FindFacility.java
@@ -1,6 +1,8 @@
 package app.usfit.api.facility;
 
+import app.usfit.api.facility.entity.Facility;
 import app.usfit.api.facility.repository.FacilityRepository;
+import app.usfit.api.facility.repository.FacilitySportRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,21 +19,64 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class FindFacility {
     @Autowired
-    FacilityRepository repo;
+    FacilityRepository facilityRepo;
+
+    @Autowired
+    FacilitySportRepository facilitySportRepo;
 
     @Test
     @DisplayName("경기도의 모든 시설을 찾는다 facility id를 반환")
     void findIdsBySido(){
-        List<Long> ids = repo.findFacilityIdsBySidoCd("4100000000");
-        System.out.println("facility ids = " + ids);
-        assertThat(ids).isNotNull();
+        List<Facility> facilities = facilityRepo.findFacilityBySidoCd("4100000000");
+        for (Facility facility : facilities) {
+            System.out.println("시설 이름: " + facility.getName());
+        }
+        assertThat(facilities).isNotNull();
     }
 
     @Test
     @DisplayName("facility type code 로 어떤 종목의 시설인지 찾고 facility id를 반환")
     void findIdsByTypeCode(){
-        List<Long> ids = repo.findFacilityIdsByTypeCd("N1701"); //농구 시설 반환
-        System.out.println("facility ids = " + ids);
-        assertThat(ids).isNotNull();
+        List<Facility> facilities = facilityRepo.findFacilityByTypeCd("N1701"); //농구 시설 반환
+        for (Facility facility : facilities) {
+            System.out.println("시설 이름: " + facility.getName());
+        }
+        assertThat(facilities).isNotNull();
+    }
+
+    @Test
+    @DisplayName("sport name으로 facility들 찾기")
+    void findFacilityBySportName() {
+        List<Facility> facilities = facilitySportRepo.findFacilitiesBySportName("농구");
+
+        for (Facility facility : facilities) {
+            System.out.println("시설 이름: " + facility.getName());
+        }
+
+        assertThat(facilities).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("sport name과 sido로 (도) facility들 찾기")
+    void findFacilityBySportNameAndSido(){
+        List<Facility> facilities = facilitySportRepo.findFacilitiesBySportNameAndSido("체력단련장", "경기도");
+        System.out.println("체력단련장 + 경기도");
+        for (Facility facility : facilities) {
+            System.out.println("시설 이름: " + facility.getName());
+        }
+
+        assertThat(facilities).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("sport name과 sigunguCd로 (시) facility 찾기")
+    void findFacilityBySportNameAndSigungu(){
+        List<Facility> facilities = facilitySportRepo.findFacilitiesBySportNameAndSigungu("간이운동장", "김천시");
+
+        for (Facility facility : facilities) {
+            System.out.println("시설 이름: " + facility.getName());
+        }
+
+        assertThat(facilities).isNotEmpty();
     }
 }
