dd12d9843fc0e30ef4524c1d7dd05f7f494ce4b4
sehyeon <tpgusdla5789@gmail.com>
2025-11-03 01:48:55 +0900

    feat: facility detail info 넘기기 위해 api 설정

--- src/main/java/app/usfit/api/facility/controller/FacilityController.java
+++ src/main/java/app/usfit/api/facility/controller/FacilityController.java
@@ -1,4 +1,24 @@
 package app.usfit.api.facility.controller;
 
+import app.usfit.api.facility.dto.FacilityDetailDto;
+import app.usfit.api.facility.repository.FacilityRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/facility")
 public class FacilityController {
+
+    private FacilityRepository facilityRepository;
+
+    public FacilityController(FacilityRepository facilityRepository){
+        this.facilityRepository = facilityRepository;
+    }
+
+    @GetMapping("/{id}")
+    public FacilityDetailDto getFacilityDetail(@PathVariable Long id){
+        return facilityRepository.getFacilityInfo(id);
+    }
 }
