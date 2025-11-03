94c2b252193e35051cb3620a35712be922e106d7
sehyeon <tpgusdla5789@gmail.com>
2025-11-04 01:00:57 +0900

    feat: sport name과 sigungu name으로 시설 찾기 api 생성

--- src/main/java/app/usfit/api/facility/controller/FacilityController.java
+++ src/main/java/app/usfit/api/facility/controller/FacilityController.java
@@ -1,24 +1,35 @@
 package app.usfit.api.facility.controller;
 
 import app.usfit.api.facility.dto.FacilityDetailDto;
+import app.usfit.api.facility.entity.Facility;
 import app.usfit.api.facility.repository.FacilityRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import app.usfit.api.facility.repository.FacilitySportRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/facility")
 public class FacilityController {
 
     private FacilityRepository facilityRepository;
+    private FacilitySportRepository facilitySportRepository;
 
-    public FacilityController(FacilityRepository facilityRepository){
+    public FacilityController(FacilityRepository facilityRepository, FacilitySportRepository facilitySportRepository){
         this.facilityRepository = facilityRepository;
+        this.facilitySportRepository = facilitySportRepository;
     }
 
     @GetMapping("/{id}")
     public FacilityDetailDto getFacilityDetail(@PathVariable Long id){
         return facilityRepository.getFacilityInfo(id);
+    }
+
+    @GetMapping("/search")
+    public List<FacilityDetailDto> getFacilitiesBySportAndSigungu(
+            @RequestParam String sportName,
+            @RequestParam String sigunguNm
+    ) {
+        return facilitySportRepository.findFacilitiesBySportNameAndSigungu(sportName, sigunguNm);
     }
 }
