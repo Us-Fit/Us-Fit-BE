eee64ec70206761f71ab8c6a54ec5dc192f72927
sehyeon <tpgusdla5789@gmail.com>
2025-11-20 21:13:40 +0900

    feat: course service, repository, dto 생성 현재 종목명, 시이름, 시군구 이름으로 필터링 기능 추가

--- src/main/java/app/usfit/api/course/repository/CourseRepository.java
+++ src/main/java/app/usfit/api/course/repository/CourseRepository.java
@@ -1,4 +1,17 @@
 package app.usfit.api.course.repository;
 
-public class CourseRepository {
+import app.usfit.api.course.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    /**
+     * 종목명 + 시도명 + 시군구명으로 강좌 검색
+     */
+    List<Course> findByItemNmAndCtprvnNmAndSignguNm(
+            String itemNm,
+            String ctprvnNm,
+            String signguNm
+    );
 }
