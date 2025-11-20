23d7d45f24c3721e9fd0d5fbcd43068244c32698
sehyeon <tpgusdla5789@gmail.com>
2025-11-20 21:11:30 +0900

    feat: course data import 실행 추가

--- src/main/java/app/usfit/api/UsFitApplication.java
+++ src/main/java/app/usfit/api/UsFitApplication.java
@@ -1,10 +1,16 @@
 package app.usfit.api;
 
+import app.usfit.api.course.service.CourseImportService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 
 @SpringBootApplication
 public class UsFitApplication {
@@ -35,4 +41,28 @@ public class UsFitApplication {
 //			System.out.println("✅ Imported rows = " + imported);
 //		};
 //	}
+    // 서버 처음 실행할 때만 주석 해제해서 import 실행
+//    @Bean
+//    @Profile("!test")
+//    CommandLineRunner importCourses(CourseImportService service) {
+//        return args -> {
+//            try{
+//                // 로컬 CSV 파일 경로
+//                Path csvPath = Path.of("C:/Users/tpgus/OneDrive/usfit-bigdata/svch/course_data.csv");
+//
+//                // CSV 인코딩 (UTF-8 또는 MS949)
+////            Charset charset = StandardCharsets.UTF_8;
+//
+//                int imported = service.importCsv(csvPath, true, StandardCharsets.UTF_8);
+//
+//                System.out.println("======================================");
+//                System.out.println("  📘 Course Data Import Completed");
+//                System.out.println("  → Imported rows: " + imported);
+//                System.out.println("======================================");
+//            } catch(Exception e){
+//                System.out.println("❌ Course import 중 예외 발생!");
+//                e.printStackTrace();  // 실제 원인 다 찍힘
+//            }
+//        };
+//    }
 }
