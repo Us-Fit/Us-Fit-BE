14b9545af4d84a119c6e641dbef516c89dd17199
sehyeon <tpgusdla5789@gmail.com>
2025-11-22 03:07:42 +0900

    feat: review 기능을 위한 entity 및 dto

--- src/main/java/app/usfit/api/review/entity/ReviewImage.java
+++ src/main/java/app/usfit/api/review/entity/ReviewImage.java
@@ -1,13 +1,21 @@
 package app.usfit.api.review.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "review_image")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ReviewImage {
-    @Id @GeneratedValue
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id")
     private Review review;
 
