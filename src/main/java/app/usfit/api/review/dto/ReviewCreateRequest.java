c7b151fb2ebd131fbdde64e0af4de2d6715ea23e
sehyeon <tpgusdla5789@gmail.com>
2025-11-25 01:50:19 +0900

    feat: review CRUD 과정에서 user validation 진행

--- src/main/java/app/usfit/api/review/dto/ReviewCreateRequest.java
+++ src/main/java/app/usfit/api/review/dto/ReviewCreateRequest.java
@@ -9,7 +9,6 @@ public class ReviewCreateRequest {
 
     private ReviewTargetType targetType;   // COURSE / FACILITY
     private Long targetId;                 // entity id
-    private Long userId;                   // 로그인 기능 붙으면 FE가 토큰에서 전달
     private Integer rating;
     private String comment;
 }
