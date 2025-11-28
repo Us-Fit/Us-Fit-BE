06bd69c8c90b38fba62e7d85a5afc00862a2c132
sehyeon <tpgusdla5789@gmail.com>
2025-11-28 21:43:30 +0900

    feat: activity 관련 repository 생성

--- src/main/java/app/usfit/api/club/repository/ClubMemberRepository.java
+++ src/main/java/app/usfit/api/club/repository/ClubMemberRepository.java
@@ -1,4 +1,11 @@
 package app.usfit.api.club.repository;
 
-public interface ClubMemberRepository {
+import app.usfit.api.club.entity.ClubMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+
+    Optional<ClubMember> findByClubIdAndUserId(Long clubId, Long userId);
 }
