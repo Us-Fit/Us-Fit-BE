481baee40b79f7e1d66a8197774398bfc2b33766
sehyeon <tpgusdla5789@gmail.com>
2025-12-01 00:50:08 +0900

    feat: course, facility 목록 조회 부분 api에서 pagination 기능 추가

--- src/main/java/app/usfit/api/course/controller/CourseController.java
+++ src/main/java/app/usfit/api/course/controller/CourseController.java
@@ -3,6 +3,8 @@ package app.usfit.api.course.controller;
 import app.usfit.api.course.dto.CourseDetailDto;
 import app.usfit.api.course.service.CourseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,6 +30,24 @@ public class CourseController {
             @RequestParam String signguNm
     ) {
         return courseService.getCoursesByFilter(itemNm, ctprvnNm, signguNm);
+    }
+
+    /**
+     * 프론트에서 3개 값(itemNm, ctprvnNm, signguNm)을 주면
+     * 그 조건에 맞는 강좌 리스트 반환
+     *
+     * 예)
+     * GET /api/courses/search?itemNm=농구&ctprvnNm=서울특별시&signguNm=강남구
+     * pagination 적용
+     */
+    @GetMapping("/search/page")
+    public Page<CourseDetailDto> searchCourses(
+            @RequestParam String itemNm,
+            @RequestParam String ctprvnNm,
+            @RequestParam String signguNm,
+            Pageable pageable
+    ) {
+        return courseService.getCoursesByFilter(itemNm, ctprvnNm, signguNm, pageable);
     }
 
     @GetMapping("/{id}")
