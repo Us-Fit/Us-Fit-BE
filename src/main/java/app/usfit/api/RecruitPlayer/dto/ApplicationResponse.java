b84c3c2d6bf9ec002fde6abd81918a5891727602
lee <151509222+yoonB-dev@users.noreply.github.com>
2025-12-02 14:31:14 +0900

    feat: 용병 전체 참여자 목록 보기 및 탈퇴 기능 추가

--- src/main/java/app/usfit/api/RecruitPlayer/dto/ApplicationResponse.java
+++ src/main/java/app/usfit/api/RecruitPlayer/dto/ApplicationResponse.java
@@ -5,9 +5,9 @@ import java.time.LocalDateTime;
 import app.usfit.api.RecruitPlayer.entity.RecruitApplication.Status;
 
 public record ApplicationResponse(
-    Long applicationId,
+    Long applicationId, 
     Long postId,
-    Long applicantId,
+    Long applicantId, // 유저 아이디임
     String introduction,
     Status status,
     LocalDateTime appliedAt
